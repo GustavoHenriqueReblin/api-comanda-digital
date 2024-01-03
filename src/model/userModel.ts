1be1eee03352f919f561bb9ad16800c05fd39905
Gustavo <gustavohique12@gmail.com>
2024-01-02 21:50:26 -0300

    fix: alteração do usuário pois no front está sendo necessário informar e-mail válido.

--- src/model/userModel.ts
+++ src/model/userModel.ts
@@ -1,7 +1,7 @@
 import { User } from '../types';
 
 export const fakeUserData: User[] = [
-    { id: 1, username: "jonas", password: "a", token: "" },
+    { id: 1, username: "jonas@gmail.com", password: "a", token: "" },
     { id: 2, username: "pedro", password: "b", token: "" },
     { id: 3, username: "alex", password: "c", token: "" }
 ];
