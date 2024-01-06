6bc9ae2a84a10f3407b9afe2be6116a3a1971438
Gustavo <gustavohique12@gmail.com>
2024-01-05 23:23:43 -0300

    fix: mais mesas no array de fake data.

--- src/model/tableModel.ts
+++ src/model/tableModel.ts
@@ -4,4 +4,9 @@ export const fakeTableData: Table[] = [
     { id: 1, code: 11, state: true },
     { id: 2, code: 12, state: false },
     { id: 3, code: 13, state: true },
+    { id: 4, code: 21, state: true },
+    { id: 5, code: 22, state: true },
+    { id: 6, code: 23, state: false },
+    { id: 7, code: 31, state: true },
+    { id: 8, code: 32, state: true },
 ];
