a9ae57b359a7914b73c386317b13d6e6b15bba57
Gustavo <gustavohique12@gmail.com>
2024-01-20 17:42:52 -0300

    fix: Deixado as mesas livres.

--- src/model/tableModel.ts
+++ src/model/tableModel.ts
@@ -2,11 +2,11 @@ import { Table } from '../types';
 
 export const fakeTableData: Table[] = [
     { id: 1, code: 11, state: true },
-    { id: 2, code: 12, state: false },
+    { id: 2, code: 12, state: true },
     { id: 3, code: 13, state: true },
     { id: 4, code: 21, state: true },
     { id: 5, code: 22, state: true },
-    { id: 6, code: 23, state: false },
+    { id: 6, code: 23, state: true },
     { id: 7, code: 31, state: true },
     { id: 8, code: 32, state: true },
 ];
